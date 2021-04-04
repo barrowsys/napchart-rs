2b9e2da5f3be9900f93e2d26fb3c48c89e1ddda5
Ezra Barrow <barrow@tilde.team>
2021-04-04 15:48:45 -0400

    initial commit of rebuild\

--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,4 @@
  * -Ezra Barrow
  * --------------------
  */
-
-use napchart::api::BlockingClient;
-
-#[allow(warnings)]
-fn main() {
-    let client = BlockingClient::default();
-    let mut new_chart = napchart::Napchart::default().lanes(1);
-    let lane = new_chart.get_lane_mut(0).unwrap();
-    lane.add_element(1, 72).unwrap();
-    lane.add_element(470, 472).unwrap();
-    lane.add_element(870, 873).unwrap();
-    lane.add_element(1270, 1274).unwrap();
-    // println!(
-    //     "{:?}",
-    //     client.create_snapshot(&new_chart, "title", "").unwrap()
-    // );
-}
-// {
-//     "chartDocument": {
-//         "chartData": {
-//             "lanes":1,
-//             "shape":"circle",
-//             "elements": [
-//                 {"end":72,"lane":0,"text":"","color":"red","start":1},{"end":472,"lane":0,"text":"","color":"red","start":470},{"end":873,"lane":0,"text":"","color":"red","start":870},{"end":1274,"lane":0,"text":"","color":"red","start":1270}
-//             ],
-//             "colorTags":[],
-//             "lanesConfig": {"0":{"locked":false}}
-//         },
-//         "chartid": "Ytce0GcXY",
-//         "title":"test",
-//         "description":"",
-//         "username":"anonymous",
-//         "lastUpdated":"2021-03-29T20:13:46.582Z",
-//         "isSnapshot":true
-//     },
-//     "publicLink": "https://napchart.com/snapshot/Ytce0GcXY"
-// }
+fn main() {}
